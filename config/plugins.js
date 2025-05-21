d848b5e850ef5c56e644baa19274f1f1dd4e8b6d
your_username <piyushamola@gmail.com>
2025-05-22 00:12:06 +0530

    added webp converter

--- config/plugins.js
+++ config/plugins.js
@@ -11,6 +11,19 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  upload: { config: { breakpoints: {} } }, // disable small/medium/large
+
+  "webp-converter": {
+    enabled: true,
+    config: {
+      mimeTypes: ["image/png", "image/jpeg", "image/jpg"],
+      options: {
+        quality: 100,
+        effort: 4,
+        smartSubsample: true,
+      },
+    },
+  },
   // "strapi-plugin-ckeditor": {
   //   enabled: true,
   // },
