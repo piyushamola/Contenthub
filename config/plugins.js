36801c87f3dad7d024896b842fb02234b2801d8d
your_username <piyushamola@gmail.com>
2025-05-24 01:19:47 +0530

    configure sendgrid

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,4 @@
 module.exports = ({ env }) => ({
-  email: {
-    config: {
-      provider: "@strapi/provider-email-sendgrid",
-      providerOptions: {
-        apiKey: env("SENDGRID_API_KEY"),
-      },
-      settings: {
-        defaultFrom: env("EMAIL_DEFAULT_FROM"),
-        defaultReplyTo: env("EMAIL_DEFAULT_REPLY_TO"),
-      },
-    },
-  },
   upload: { config: { breakpoints: {} } }, // disable small/medium/large
 
   "webp-converter": {
