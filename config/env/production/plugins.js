7f77485cca76f1b4f21e8185aacff82842b78af9
your_username <piyushamola@gmail.com>
2026-09-19 14:15:50 +0530

    changed the mail provider

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,19 +1,31 @@
 module.exports = ({ env }) => ({
+  // email: {
+  //   config: {
+  //     provider: "@strapi/provider-email-nodemailer",
+  //     providerOptions: {
+  //       host: "smtp-relay.brevo.com", // Brevo SMTP gateway
+  //       port: 587, // use 465 if you need SSL-only
+  //       secure: false, // STARTTLS on 587
+  //       auth: {
+  //         user: env("BREVO_SMTP_USER"), // usually same as your login email
+  //         pass: env("BREVO_SMTP_KEY"), // your SMTP key
+  //       },
+  //     },
+  //     settings: {
+  //       defaultFrom: env("EMAIL_DEFAULT_FROM"),
+  //       defaultReplyTo: env("EMAIL_DEFAULT_REPLY_TO"),
+  //     },
+  //   },
+  // },
   email: {
     config: {
-      provider: "@strapi/provider-email-nodemailer",
+      provider: "sendgrid",
       providerOptions: {
-        host: "smtp-relay.brevo.com", // Brevo SMTP gateway
-        port: 587, // use 465 if you need SSL-only
-        secure: false, // STARTTLS on 587
-        auth: {
-          user: env("BREVO_SMTP_USER"), // usually same as your login email
-          pass: env("BREVO_SMTP_KEY"), // your SMTP key
-        },
+        apiKey: env("SENDGRID_API_KEY"),
       },
       settings: {
-        defaultFrom: env("EMAIL_DEFAULT_FROM"),
-        defaultReplyTo: env("EMAIL_DEFAULT_REPLY_TO"),
+        defaultFrom: "wishhappybday@gmail.com",
+        defaultReplyTo: "wishhappybday@gmail.com",
       },
     },
   },
--- config/plugins.js
+++ config/plugins.js
@@ -12,25 +12,38 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  email: {
-    config: {
-      provider: "@strapi/provider-email-nodemailer",
-      providerOptions: {
-        host: "smtp-relay.brevo.com", // Brevo SMTP gateway
-        port: 587, // use 465 if you need SSL-only
-        secure: false, // STARTTLS on 587
-        auth: {
-          user: env("BREVO_SMTP_USER"), // usually same as your login email
-          pass: env("BREVO_SMTP_KEY"), // your SMTP key
-        },
-      },
-      settings: {
-        defaultFrom: env("EMAIL_DEFAULT_FROM"),
-        defaultReplyTo: env("EMAIL_DEFAULT_REPLY_TO"),
-      },
-    },
-  },
+  // email: {
+  //   config: {
+  //     provider: "@strapi/provider-email-nodemailer",
+  //     providerOptions: {
+  //       host: "smtp-relay.brevo.com", // Brevo SMTP gateway
+  //       port: 587, // use 465 if you need SSL-only
+  //       secure: false, // STARTTLS on 587
+  //       auth: {
+  //         user: env("BREVO_SMTP_USER"), // usually same as your login email
+  //         pass: env("BREVO_SMTP_KEY"), // your SMTP key
+  //       },
+  //     },
+  //     settings: {
+  //       defaultFrom: env("EMAIL_DEFAULT_FROM"),
+  //       defaultReplyTo: env("EMAIL_DEFAULT_REPLY_TO"),
+  //     },
+  //   },
+  // },
   // "strapi-plugin-ckeditor": {
   //   enabled: true,
   // },
+
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "wishhappybday@gmail.com",
+        defaultReplyTo: "wishhappybday@gmail.com",
+      },
+    },
+  },
 });
